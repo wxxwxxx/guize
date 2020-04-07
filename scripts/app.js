5b798f27d99b88bfc08daef21b340c5247b1571c
wxxwxxx <zhangxiaowena@gmail.com>
2020-04-08 03:23:24 +0800

    xx

--- scripts/app.js
+++ scripts/app.js
@@ -2659,11 +2659,8 @@ function makeConf(params) {
       function ssr2ss(proxies) {
         let proxyList = proxies.split(/\n/);
         let res = proxyList.map(proxy => {
-          if (/=\s*shadowsocksr/.test(proxy)) {
-              return proxy.replace(/=\s*shadowsocksr/g, '= ss').replace(/"/g, '').replace(/\s*(none|aes|rc4|salsa20|chacha20)[^,$]+/g, 'encrypt-method=$&').replace(/\wenwenai2/g, 'password=$&').replace(/,\s*(protocol|protocol_param|obfs|obfs_param)[^,$]+/g, '')
-          }
-          else if (/=\s*custom/.test(proxy)) {
-            return proxy.replace(/=\s*custom/g, '= ss').replace(/\s*(none|aes|rc4|salsa20|chacha20)[^,$]+/g, 'encrypt-method=$&').replace(/\wenwenai2/g, 'password=$&').replace(/,\s*[https://github.com/lhie1/Rules/blob/master/SSEncrypt.module?raw=true]/g, '')
+          if (/=\s*custom/.test(proxy)) {
+            return proxy.replace(/=\s*custom/g, '= ss').replace(/\s*(none|aes|rc4|salsa20|chacha20)[^,$]+/g, 'encrypt-method=$&').replace(/\wenwenai2/g, 'password=$&').replace(/,\s*(https)[^]+/g, '')
           }
           else if (/=\s*vmess/ && /obfs=ws/.test(proxy)) {
               return proxy.replace(/"/g, '').replace(/\s*(none|aes|rc4|salsa20|chacha20)[^,$]+,/g, 'username=').replace(/(group=)[^]+/g, ' ws=true, ws-path= /')
